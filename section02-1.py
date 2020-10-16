1ecc06775e97005af63eadc85b3758b8e3c252ad
Lucy Ter <pah0729@gmail.com>
2020-10-16 14:32:34 +0900

    파이썬 기초

---
+++ section02-1.py
@@ -0,0 +1,24 @@
+# Section02-1
+# 파이썬 기초 코딩
+# Print 구문의 이해
+
+# 기본출력
+print('Hello Python!')
+print("Hello Python!")
+print("""Hello Python""")
+print('''Hello Python''')
+
+print()
+
+# Separator 옵션 사용
+print('T', 'E', 'S', 'T', sep='')
+print('2020', '10', '16', sep='-')
+print('admin', 'test.com', sep='@')
+
+print()
+
+# end 옵션 사용
+print('Welcome To', end='')
+print('the black parada', end=' ')
+print('piano notes')
+
